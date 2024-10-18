fce3f6c8e4a88df5cde1d7f0ad93dbdd20fc2675
Kai Hatje <kai.hatje@app-akademie.com>
2024-10-18 09:25:05 +0200

    Added some of the theme colors.

---
+++ lib/config/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color rocketBlue = Color.fromARGB(255, 70, 175, 219);
+const Color extremelyLightBlue = Color.fromARGB(255, 226, 222, 254);
+const Color lightBlack = Color.fromRGBO(255, 255, 255, .48);
