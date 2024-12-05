3419ae1a2d2a6748450d8a887473296a982f26d4
Muhammad <muhannmmad@gmail.com>
2024-12-05 09:34:58 +0100

    changed the main color to grean to look better

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -4,5 +4,5 @@ const Color lightBlue = Color.fromRGBO(140, 180, 240, .8);
 const Color rocketBlue = Color.fromARGB(255, 70, 175, 219);
 const Color darkBlue = Color.fromRGBO(40, 60, 180, .69);
 const Color veryLightBlue = Color.fromRGBO(140, 180, 240, .42);
-const Color extremelyLightBlue = Color.fromARGB(255, 226, 222, 254);
+const Color extremelyLightBlue = Color.fromARGB(255, 31, 216, 10);
 const Color lightBlack = Color.fromRGBO(255, 255, 255, .48);
